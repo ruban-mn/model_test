9e65a3af9c9819d9fd44c3577de46cabdc356a15
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 16:46:06 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,52 +8,10 @@
 
 import streamlit as st
 
-def main():
-    st.header("Мастер отчетов независимой оценки качества услуг")
-    
-    session_state = st.session_state
-    if 'button_pressed' not in session_state:
-        session_state.button_pressed = False
-
-    menu = ["Главная", "Контакты"]
-    choice = st.sidebar.selectbox("Menu", menu)
-
-    if choice == "Главная":
-        st.header("Вас приветствует мастер отчетов независимой оценки качества услуг")
-        st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
+st.header("Вас приветствует мастер отчетов независимой оценки качества услуг")
+st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
        
-        if st.button('Расчеты для организаций культуры'):
-            session_state.button_pressed = True
-            session_state.selected_option = 'organization_culture'
-        
-        if st.button('Расчеты для социальных организаций'):
-            session_state.button_pressed = True
-            session_state.selected_option = 'social_organization'
+    if st.button('Расчеты для организаций культуры'):
+        st.page_link("pages/myself_an_analitik_8_2.py", label="Расчеты для организаций культуры")
 
-        if st.button('Расчеты для медицинских организаций'):
-            session_state.button_pressed = True
-            session_state.selected_option = 'medical_organization'
-    
-    elif choice == "Контакты":
-        st.write("Разработчик Рубан Мария Николаевна, Ma77ruban@yandex.ru")
-        # Add content for the 'Contact' page here
-
-    if session_state.button_pressed:
-        if session_state.selected_option == 'organization_culture':
-            with open('myself_an_analitik_8_2.py', 'r') as file:
-                code = file.read()
-            exec(code)
-        
-        if session_state.selected_option == 'social_organization':
-            with open('myself_an_analitik_8_3.py', 'r') as file:
-                code = file.read()
-            exec(code)
-
-        if session_state.selected_option == 'medical_organization':
-            with open('myself_an_analitik_8_4.py', 'r') as file:
-                code = file.read()
-            exec(code)
-
-if __name__ == '__main__':
-    main()
 
