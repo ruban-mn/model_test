cbb994c2674ef56ed6ef29435da2e62f431edc8c
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 12:03:44 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,15 @@ def main():
     if choice == "Главная":
         st.write("Вас приветствует мастер отчетов независимой оценки качества услуг")
         st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
+       
+# Получение конфигурации текущей страницы
+        page_config = st.api.get_page_config()
 
+# Вывод информации о текущей странице
+        st.write("URL страницы:", page_config.url)
+        st.write("Заголовок страницы:", page_config.title)
+        st.write("Ширина страницы:", page_config.width)
+        st.write("Высота страницы:", page_config.height)
         st.page_link('https://github.com/ruban-mn/model1/blob/pages/myself_an_analitik_8_2.py', label='Расчеты для организаций культуры')
         st.page_link('https://github.com/ruban-mn/model1/blob/pages/myself_an_analitik_8_3.py', label='Расчеты для социальных организаций')
         st.page_link('https://github.com/ruban-mn/model1/blob/pages/myself_an_analitik_8_4.py', label='Расчеты для медицинских организаций')
