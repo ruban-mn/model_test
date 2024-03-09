ed1a3a28e065cc1d845b64668acf08c5a76214e8
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 15:11:57 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,41 +8,53 @@
 
 import streamlit as st
 
+class SessionState:
+    def __init__(self, **kwargs):
+        self._state = kwargs
+
 def main():
     st.title("Мастер отчетов независимой оценки качества услуг")
+    
+    session_state = SessionState(button_pressed=False)
 
     menu = ["Главная", "Контакты"]
     choice = st.sidebar.selectbox("Меню", menu)
 
-    
     if choice == "Главная":
-        st.subheader("Вас приветствует мастер отчетов независимой оценки качества услуг")
+        st.write("Вас приветствует мастер отчетов независимой оценки качества услуг")
         st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
-
-        state = st.session_state.get('state', {'redirect': False})
+       
         if st.button('Расчеты для организаций культуры'):
-            with open('./myself_an_analitik_8_2.py', 'r') as file:
-                code = file.read()
-            exec(code)
-        # Устанавливаем флаг перенаправления на другую страницу
-            state['redirect'] = True
-        # Проверяем, нужно ли перенаправить пользователя
-        #if state.get('redirect', False):
-           #st.experimental_rerun()
+            session_state.button_pressed = True
+            session_state.selected_option = 'organization_culture'
         
         if st.button('Расчеты для социальных организаций'):
-            with open('./myself_an_analitik_8_3.py', 'r') as file:
-                code = file.read()
-            exec(code)
+            session_state.button_pressed = True
+            session_state.selected_option = 'social_organization'
 
         if st.button('Расчеты для медицинских организаций'):
-            with open('./myself_an_analitik_8_4.py', 'r') as file:
+            session_state.button_pressed = True
+            session_state.selected_option = 'medical_organization'
+    
+    elif choice == "Контакты":
+        st.write("Страница с контактной информацией.")
+        # Добавьте содержимое для страницы 'Контакты' здесь
+
+    if session_state.button_pressed:
+        if session_state.selected_option == 'organization_culture':
+            with open('myself_an_analitik_8_2.py', 'r') as file:
+                code = file.read()
+            exec(code)
+        
+        if session_state.selected_option == 'social_organization':
+            with open('myself_an_analitik_8_3.py', 'r') as file:
                 code = file.read()
             exec(code)
 
-    elif choice == "Контакты":
-        st.write("Страница с контактной информацией.")
-        # Вставьте здесь содержимое для страницы 'Контакты'
+        if session_state.selected_option == 'medical_organization':
+            with open('myself_an_analitik_8_4.py', 'r') as file:
+                code = file.read()
+            exec(code)
 
 if __name__ == '__main__':
     main()
