2857e60e7bc026a8bb812711c9ffc843de8627ad
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 13:15:38 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ def main():
        
         if st.button('Расчеты для организаций культуры'):
             with open('./pages/myself_an_analitik_8_2.py', 'r') as file:
-                code = file.read('myself_an_analitik_8_2.py')
+                code = file.read()
             exec(code)
         
         if st.button('Расчеты для социальных организаций'):
