ea15e36d189ac21347977e9201ad3f984a068775
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 23:13:16 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,8 +5,6 @@ import streamlit as st
 st.header("Вас приветствует мастер отчетов независимой оценки качества услуг")
 st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
 
-if st.button("Главная"):
-    st.switch_page("Home.py")
 if st.button("Расчет для организаций культуры"):
     st.switch_page("pages/myself_an_analitik_8_2.py")
 if st.button("Расчет для социальных организаций"):
@@ -14,3 +12,5 @@ if st.button("Расчет для социальных организаций"):
 if st.button("Расчет для медицинских организаций"):
     st.switch_page("pages/myself_an_analitik_8_4.py")
        
+st.sidebar.[Главная]
+st.sidebar.[страница1]
