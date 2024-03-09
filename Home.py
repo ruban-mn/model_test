575c3cfbb1e853f83800cde94a992c8a1a421317
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-10 00:38:25 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,6 +6,7 @@ st.title("Вас приветствует")
 st.header(":orange[мастер отчетов независимой оценки качества услуг]")
 st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='rainbow')
 
+st.markdown('<button style="font-weight: bold;">Расчет для организаций культуры</button>', unsafe_allow_html=True)
 if st.button("Расчет для организаций культуры"):
     st.switch_page("pages/Для_организаций_культуры.py")
 if st.button("Расчет для социальных организаций"):
