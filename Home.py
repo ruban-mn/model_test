3e4870bf79e9404dc03dd85803514c83d535d47f
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-10 00:46:07 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,11 +4,14 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Вас приветствует</h1>", unsafe_allow_html=True)
 
-st.header(":orange[мастер отчетов независимой оценки качества услуг]")
+st.header(":orange[мастер отчетов]")
+st.header(":orange[независимой оценки качества услуг]")
 st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='rainbow')
 
+st.subheader(':red[перед загрузкой файлов удетитесь что они соответствуют образцу]:', divider='rainbow')
+st.subheader('образцы можно посмотреть в меню слева - образцы для загрузочных файлов', divider='rainbow')
 
-if st.button('<button style="font-weight: bold;">Расчет для организаций культуры</button>', unsafe_allow_html=True):
+if st.button("Расчет для организаций культуры"):
     st.switch_page("pages/Для_организаций_культуры.py")
 if st.button("Расчет для социальных организаций"):
     st.switch_page("pages/Для_социальных_организаций.py")
