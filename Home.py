78594c4e6a4cee7ed49e89628e0434c9bca7ae13
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-10 01:17:49 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,14 +4,13 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Вас приветствует</h1>", unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center;'>:orange[мастер отчетов]</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>мастер отчетов</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>независимой оценки качества услуг</h2>", unsafe_allow_html=True)
 
-st.header(":orange[мастер отчетов]")
-st.header(":orange[независимой оценки качества услуг]")
-st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='rainbow')
+st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='red')
 
-st.subheader(':red[перед загрузкой файлов удетитесь что они соответствуют образцу]:', divider='rainbow')
-st.subheader('образцы можно посмотреть в меню слева - образцы для загрузочных файлов', divider='rainbow')
+st.subheader(':red[перед загрузкой файлов удетитесь что они соответствуют образцу]:', divider='red')
+st.subheader('образцы можно посмотреть в меню слева - образцы для загрузочных файлов')
 
 if st.button("Bold[Расчет для организаций культуры]"):
     st.switch_page("pages/Для_организаций_культуры.py")
