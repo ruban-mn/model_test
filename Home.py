f87150f24312d5085cf958db9cb5e7c8708dc04c
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-10 01:34:36 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,18 +4,18 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Вас приветствует</h1>", unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center;'>мастер отчетов</h2>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;'>независимой оценки качества услуг</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;color:orange;'>мастер отчетов</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;color:orange;'>независимой оценки качества услуг</h2>", unsafe_allow_html=True)
 
 st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='red')
 
 st.subheader(':red[перед загрузкой файлов убедитесь что они соответствуют образцу]:', divider='red')
 st.subheader('образцы можно посмотреть в меню слева - образцы для загрузочных файлов')
 
-if st.button("Расчет для организаций культуры"):
+if st.button("**Расчет для организаций культуры**"):
     st.switch_page("pages/Для_организаций_культуры.py")
-if st.button("Расчет для социальных организаций"):
+if st.button("**Расчет для социальных организаций**"):
     st.switch_page("pages/Для_социальных_организаций.py")
-if st.button("Расчет для медицинских организаций"):
+if st.button("**Расчет для медицинских организаций**"):
     st.switch_page("pages/Для_медицинских_организаций.py")
        
