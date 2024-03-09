83fda1d36654feb444c899b09957563a047a5265
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-10 00:42:10 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,14 +2,13 @@
 
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>Текст выровнен по центру</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Вас приветствует</h1>", unsafe_allow_html=True)
 
-st.title("Вас приветствует")
 st.header(":orange[мастер отчетов независимой оценки качества услуг]")
 st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='rainbow')
 
-st.markdown('<button style="font-weight: bold;">Расчет для организаций культуры</button>', unsafe_allow_html=True)
-if st.button("Расчет для организаций культуры"):
+
+if st.button('<button style="font-weight: bold;">Расчет для организаций культуры</button>', unsafe_allow_html=True):
     st.switch_page("pages/Для_организаций_культуры.py")
 if st.button("Расчет для социальных организаций"):
     st.switch_page("pages/Для_социальных_организаций.py")
