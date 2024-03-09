ab3425be76ca4df0f6e09b2f80c87361883678c2
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 21:04:00 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ import streamlit as st
 st.header("Вас приветствует мастер отчетов независимой оценки качества услуг")
 st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
        
-if st.button('Расчеты для организаций культуры'):
-       st.page_link('pages/myself_an_analitik_8_2.py', label="Расчеты для организаций культуры")
+st.button('Расчеты для организаций культуры'):
+st.page_link('pages/myself_an_analitik_8_2.py', label="Расчеты для организаций культуры")
 
 
