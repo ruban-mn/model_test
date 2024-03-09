6694c1e3d8b66b43fa280f812c4cf7159e452205
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 11:32:52 +0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# app.py - главный файл приложения
+
+import streamlit as st
+
+def main():
+    st.title("Мастер отчетов независимой оценки качества услуг")
+
+    menu = ["Главная", "Контакты"]
+    choice = st.sidebar.selectbox("Меню", menu)
+
+    if choice == "Главная":
+        st.write("Вас приветствует мастер отчетов независимой оценки качества услуг")
+        st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
+
+        st.page_link("pages/myself_an_analitik_8_2.py", label="Расчеты для организаций культуры")
+        st.page_link("pages/myself_an_analitik_8_3.py", label="Расчеты для социальных организаций")
+        st.page_link("pages/myself_an_analitik_8_4.py", label="Расчеты для медицинских организаций")
+
+    elif choice == "Контакты":
+        st.write("Страница с контактной информацией.")
+        # Вставьте здесь содержимое для страницы 'Контакты'
+
+if __name__ == '__main__':
+    main()
+
