e742d8ef09c2b8fe433fb21055da250f6b6bb833
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-10 01:35:55 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,12 +2,12 @@
 
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>Вас приветствует</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;color:orange;'>Вас приветствует</h1>", unsafe_allow_html=True)
 
 st.markdown("<h2 style='text-align: center;color:orange;'>мастер отчетов</h2>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;color:orange;'>независимой оценки качества услуг</h2>", unsafe_allow_html=True)
 
-st.subheader(':blue[перейдите на страницу с требуемым расчетом]:', divider='red')
+st.subheader('перейдите на страницу с требуемым расчетом', divider='red')
 
 st.subheader(':red[перед загрузкой файлов убедитесь что они соответствуют образцу]:', divider='red')
 st.subheader('образцы можно посмотреть в меню слева - образцы для загрузочных файлов')
