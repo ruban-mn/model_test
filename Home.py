d1d28a864ef09a37a073e46566f82dbfc0c562f4
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-03-09 14:26:35 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,7 +19,7 @@ def main():
         st.subheader('перейдите на страницу с требуемым расчетом', divider='rainbow')
        
         if st.button('Расчеты для организаций культуры'):
-            st.page_link('./myself_an_analitik_8_2.py', label='Расчеты для организаций культуры')
+            st.page_link('myself_an_analitik_8_2.py', label='Расчеты для организаций культуры')
         
         if st.button('Расчеты для социальных организаций'):
             with open('./myself_an_analitik_8_3.py', 'r') as file:
