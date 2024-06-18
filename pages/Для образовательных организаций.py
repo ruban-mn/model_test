57736dedc4ee90a18738caf833db8ebdd3297b4e
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-06-18 22:10:28 +0300

    Update Для образовательных организаций.py

--- pages/Для образовательных организаций.py
+++ pages/Для образовательных организаций.py
@@ -46,7 +46,7 @@ tm.sleep(30)
 Answers_respond_list = Answers_respond.columns.tolist() ##извлекаем наименования столбцов в список
 
 New_col = []  # Создаем пустой список
-for i in range(15+1):  # Цикл от 0 до 18
+for i in range(15):  # Цикл от 0 до 18
     sim = i   # присваиваем номер
     New_col.append('v' + str(sim))  # добавляем новый номер вопрса в список
 
