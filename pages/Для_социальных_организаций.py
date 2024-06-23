d2fb0de8cb2eeed97eacaccc721bc45fac3b4592
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-06-23 23:47:44 +0300

    Update Для_социальных_организаций.py

--- pages/Для_социальных_организаций.py
+++ pages/Для_социальных_организаций.py
@@ -60,7 +60,7 @@ tm.sleep(30)
 Answers_respond_list = Answers_respond.columns.tolist() ##извлекаем наименования столбцов в список
 
 New_col = []  # Создаем пустой список
-for i in range(15):  # Цикл от 0 до 18
+for i in range(16):  # Цикл от 0 до 18
     sim = i   # присваиваем номер
     New_col.append('v' + str(sim))  # добавляем новый номер вопрса в список
 
@@ -73,7 +73,7 @@ ans_res = pd.DataFrame({'v0': Answers_respond['v0'].unique()})
 
 # Используем цикл для подсчета значений и создания новых столбцов
 for col in New_col:
-    value = 'Да'  # Значение, которое мы считаем
+    value = startswith('Да')  # Значение, которое мы считаем
     count_col_name = f'_{col}_'
     counts = Answers_respond[Answers_respond[col] == value].groupby('v0').size().reset_index(name=count_col_name)
     ans_res = ans_res.merge(counts, on='v0', how='left')
