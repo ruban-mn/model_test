94fa35da5a6de18666f68a87351eb2fd262841e2
ruban-mn <142448582+ruban-mn@users.noreply.github.com>
2024-07-24 07:33:20 +0300

    Update Для_организаций_культуры.py

--- pages/Для_организаций_культуры.py
+++ pages/Для_организаций_культуры.py
@@ -519,7 +519,7 @@ for i in range(sorted_table.shape[0]):
         table6.cell(i+1, j).text = str(sorted_table.values[i, j])
 
 abz45 = otchet.add_paragraph(f"Итоговый анализ и оценка качества работы учреждений культуры позволяет определить лучшие учреждения по результатам мониторинга.  Общий балл организаций варьируются от {min_value} до {max_value} баллов. Средний итоговый балл по сумме критериев {mean_value}.")
-abz46 = otchet.add_paragraph("Среди учреждений культуры муниципального образования Гулькевичского района в первую тройку лидеров вошли следующие учреждения:")
+abz46 = otchet.add_paragraph("Среди учреждений культуры Вологодской области в первую тройку лидеров вошли следующие учреждения:")
 for index, row in top_3_rows.iterrows():
     otchet.add_paragraph(f"{row['Наименование организации или П/Н по списку']}, {row['Общий балл']}балла.") 
 abz47 = otchet.add_paragraph("Последнюю строку рейтинга занимает")
@@ -585,16 +585,14 @@ table20.style = 'Table Grid'  # Применяем стиль таблицы
 hdr_cells = table20.rows[0].cells
 hdr_cells[0].text = 'Наименование организации'
 hdr_cells[1].text = 'Балл'
-hdr_cells[2].text = 'Рейтинг'
-hdr_cells[3].text = 'Недостатки'
+hdr_cells[2].text = 'Недостатки'
 
 # Проходимся по каждой строке и добавляем данные в таблицу
 for index, row in output_df.iterrows():
     row_cells = table20.add_row().cells
     row_cells[0].text = str(row['Name_org'])
     row_cells[1].text = str(Raschet_ballov.loc[Raschet_ballov['Наименование организации или П/Н по списку'] == row['Name_org'], 'Общий балл'].values[0])
-    row_cells[2].text = str(sorted_table.loc[sorted_table['Наименование организации или П/Н по списку'] == row['Name_org'], 'рейтинг'].values[0])
-    row_cells[3].text = f"Недостатки на стенде: отсутствуют документы о {str(row['bad_stend'])}\n"\
+    row_cells[2].text = f"Недостатки на стенде: отсутствуют документы о {str(row['bad_stend'])}\n"\
                         f"Недостатки на сайте: отсутствуют документы о {str(row['bad_sait'])}\n"\
                         f"Недостатки функционирование дистанционных способов связи: {str(row['bad_dist'])}\n"\
                         f"Недостатки комфортности условий предоставления услуг: {str(row['bad_komf'])}\n"\
